0515b5cf74cd3457c6a06b02719a977476d47d79
Ethan Harmon <ethanharmon2018@gmail.com>
2024-03-03 20:15:26 -0500

    Migrated project from personal to org

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::process;
+use std::env;
+
+use dotcomfy::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = dotcomfy::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
