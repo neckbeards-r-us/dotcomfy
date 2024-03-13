9adbdf2d6e2603965587b6e91cbcd10a95fda5d1
Ethan Harmon <ethanharmon2018@gmail.com>
2024-03-13 19:21:25 -0400

    Initial logic for clap, trying to figure out how to properly use it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-use std::process;
+use clap::{ArgMatches, Parser};
 use std::env;
+use std::process;
 
 use dotcomfy::Config;
+
+#[derive(Parser, Debug)]
+#[command(
+    version,
+    author="Ethan Harmon, Stephen Reaves",
+    about="A simple tool for managing your dotfiles",
+    long_about = None
+)]
+pub struct Args {
+    #[arg(short, long)]
+    install: String, // needs to be optional
+    repo: String, // needs to be a sub command
+                  // TODO: Am I supposed to write these options with annotations or the format on the website??
+                  // TODO: Use argmatches to see if they passed in a github username, or a repo URL
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
