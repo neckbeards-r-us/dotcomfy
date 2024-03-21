f9c4901c805043ba7ddf52e1241705a92065e51a
Ethan Harmon <ethanharmon2018@gmail.com>
2024-03-20 23:39:17 -0400

    Started work on parsing install argument (is it a GH username or repo URL?)

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,55 @@
-use clap::{ArgMatches, Parser};
+use clap::{Parser, Subcommand};
 use std::env;
 use std::process;
 
-use dotcomfy::Config;
+// use dotcomfy::Config;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(
     version,
     author="Ethan Harmon, Stephen Reaves",
     about="A simple tool for managing your dotfiles",
     long_about = None
 )]
-pub struct Args {
-    #[arg(short, long)]
-    install: String, // needs to be optional
-    repo: String, // needs to be a sub command
-                  // TODO: Am I supposed to write these options with annotations or the format on the website??
-                  // TODO: Use argmatches to see if they passed in a github username, or a repo URL
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Install { repo: Option<String> },
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
+    match &args.command {
+        Commands::Install { repo } => {
+            // TODO: Figure out how to grab value from Option and check to see if it's username or
+            // repo
+            let repo = Some(repo).unwrap_or_else(|| println!("No username/repo passed"));
+            println!("{:?}", repo)
+            /*
+            if *repo.contains("https://") {
+                println!("{repo}")
+            } else {
+                println!("Didn't pass in a repo or GH username!")
+            }
+            */
+        }
+    }
+    /*
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+    */
 
+    /*
     if let Err(e) = dotcomfy::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
+    */
 }
