a665670592f65b3d7392b717eef552fbec5b2a63
Ethan Harmon <ethanharmon2018@gmail.com>
2024-03-23 23:02:02 -0400

    Started moving logic over to lib.rs. Will use main.rs to parse CLI args and call the appropriate functions in lib.rs. Added a path subcommand (need to make it a flag) to the install command. Needs to be optional.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,39 @@
-use std::error::Error;
-use std::fs;
-use std::env;
+use git2::Repository;
 
-use clap::Parser;
+pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
+    println!("Installing {repo_url:?} at {path:?}");
+    let dot_files_path = "/tmp/dotfiles";
+    let path: String = if let Some(path) = &path {
+        path.to_string()
+    } else {
+        String::from('.')
+    };
+    let _repo: Repository = if let Some(repo_url) = &repo_url {
+        if repo_url.starts_with("https://")
+        /* && repo_url.contains("/dotfiles.git")*/
+        {
+            println!("Custom repo");
+            match Repository::clone(repo_url, dot_files_path) {
+                Ok(repo) => repo,
+                Err(e) => panic!("Failed to clone: {}", e),
+            }
+        } else {
+            println!("Default repo");
+            let repo_url = format!("https://github.com/{}/dotfiles.git", repo_url);
+            match Repository::clone(&repo_url, dot_files_path) {
+                Ok(repo) => repo,
+                Err(e) => panic!("Failed to clone: {}", e),
+            }
+        }
+    } else {
+        println!("Creating new repo at {dot_files_path}");
+        Repository::init(dot_files_path).expect("Could not create dotiles")
+    };
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-pub struct Args {
-    #[arg(short, long)]
-    config: String
+    // let checkout = git2::build::CheckoutBuilder::new();
+    //
+    // let repo_head = repo.checkout_head(checkout);
+    // println!("{:?}", repo_url.unwrap())
+    // let head = repo.head().expect("So no head?");
+    // repo.checkout_head(head.into());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use std::process;
-use std::env;
+use clap::{Parser, Subcommand};
+use git2::Repository;
 
-use dotcomfy::Config;
+use dotcomfy::install_repo;
+
+#[derive(Parser)]
+#[command(
+    version,
+    author="Ethan Harmon, Stephen Reaves",
+    about="A simple tool for managing your dotfiles",
+    long_about = None
+)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Install the repo
+    Install {
+        /// repo_url can be a full url, or just a username, or left empty
+        repo_url: Option<String>,
+        /// path to install dotfiles. relative to the current folder
+        path: Option<String>,
+    },
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let dot_file_path = "/tmp/dotfiles";
+
+    match &args.command {
+        Commands::Install { repo_url, path } => install_repo(repo_url, path),
+    }
+    /*
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
