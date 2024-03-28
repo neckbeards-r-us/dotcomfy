63aa51823876ebea3b5d285ca51341992508ab3a
Ethan Harmon <ethanharmon2018@gmail.com>
2024-03-27 21:25:07 -0400

    Started rename_files function

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,17 @@ use git2::Repository;
 
 pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
     println!("Installing {repo_url:?} at {path:?}");
-    let dot_files_path = "/tmp/dotfiles";
+    let tmp_path = "/tmp/dotfiles";
 
     // @REF [Path vs PathBuf](https://nick.groenen.me/notes/rust-path-vs-pathbuf/)
-    let mut home_path = dirs::config_local_dir().unwrap();
+    let mut dot_files_path = dirs::config_local_dir().unwrap();
 
     if let Some(path) = path {
-        home_path = PathBuf::from(path);
+        dot_files_path = PathBuf::from(path);
     } else if let Some(cfg) = dirs::config_local_dir() {
-        home_path = cfg;
+        dot_files_path = cfg;
     } else if let Some(cfg) = dirs::config_dir() {
-        home_path = cfg;
+        dot_files_path = cfg;
     }
 
     let _repo: Repository = if let Some(repo_url) = &repo_url {
@@ -26,26 +26,27 @@ pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
         /* && repo_url.contains("/dotfiles.git")*/
         {
             println!("Custom repo");
-            match Repository::clone(repo_url, dot_files_path) {
+            match Repository::clone(repo_url, tmp_path) {
                 Ok(repo) => repo,
                 Err(e) => panic!("Failed to clone: {}", e),
             }
         } else {
             println!("Default repo");
             let repo_url = format!("https://github.com/{}/dotfiles.git", repo_url);
-            match Repository::clone(&repo_url, dot_files_path) {
+            match Repository::clone(&repo_url, tmp_path) {
                 Ok(repo) => repo,
                 Err(e) => panic!("Failed to clone: {}", e),
             }
         }
     } else {
-        println!("Creating new repo at {dot_files_path}");
-        Repository::init(dot_files_path).expect("Could not create dotiles")
+        println!("Creating new repo at {tmp_path}");
+        Repository::init(tmp_path).expect("Could not create dotiles")
     };
 
-    // let _file_renaming = visit_dirs(home_path.as_path(), &rename_files);
+    let _file_renaming = rename_files(dot_files_path.as_path(), Path::new(tmp_path));
 
-    // TODO: rename old config folder and move files in repo to be new config files
+    // TODO: Work with std::fs::{read_dir, rename} to rename all old config files that would be
+    // overwritten.
 
     // let checkout = git2::build::CheckoutBuilder::new();
     //
@@ -55,10 +56,11 @@ pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
     // repo.checkout_head(head.into());
 
     // Cleanup
-    // let _remove_tmp_dir = fs::remove_dir_all(dot_files_path);
+    // let _remove_tmp_dir = fs::remove_dir_all(tmp_path);
 }
 
 // TODO: Look at changing &Path to AsRef<Path>
+/*
 fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> io::Result<()>) -> io::Result<()> {
     if dir.is_dir() {
         println!("{dir:?}");
@@ -74,8 +76,20 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> io::Result<()>) -> io::Resul
     }
     Ok(())
 }
+*/
 
-fn rename_files(entry: &DirEntry) -> io::Result<()> {
+// TODO: Turn this spaghetti into something that actually works.
+fn rename_files(dot_files_path: &Path, tmp_path: &Path) -> io::Result<()> {
+    for entry in fs::read_dir(tmp_path)? {
+        let new_path = entry?;
+        let new_entry = new_path.path().file_name();
+        let old_path = dot_files_path.join(new_path.unwrap());
+        match old_path.try_exists() {
+            Ok(true) => fs::rename(old_path, new_path),
+            Ok(false) => fs::rename(old_path, new_path),
+            Err(e) => panic!("Failed to rename: {}", e),
+        }
+    }
     println!("{entry:?}");
     Ok(())
 }
