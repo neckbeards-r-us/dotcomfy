9fcc1569047779612cf09462bed02620802a4302
Ethan Harmon <ethanharmon2018@gmail.com>
2024-07-11 16:03:26 -0400

    Realized that I have to check every item recursively instead of just surface level files/dirs. Started rename_files_recursively function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use git2::Repository;
 use std::{
+    ffi::{OsStr, OsString},
     fs::{self, DirEntry},
     io,
     path::{Path, PathBuf},
 };
-
-use git2::Repository;
+use walkdir::WalkDir;
 
 pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
     println!("Installing {repo_url:?} at {path:?}");
@@ -44,7 +45,8 @@ pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
         Repository::init(tmp_path).expect("Could not create dotiles")
     };
 
-    let _file_renaming = rename_files(dot_files_path.as_path(), Path::new(tmp_path));
+    // let _file_renaming = rename_files(&dot_files_path, &PathBuf::from(tmp_path));
+    let _file_renaming = rename_files_recursively(&dot_files_path, &PathBuf::from(tmp_path));
 
     // TODO: Work with std::fs::{read_dir, rename} to rename all old config files that would be
     // overwritten.
@@ -57,7 +59,7 @@ pub fn install_repo(repo_url: &Option<String>, path: &Option<String>) {
     // repo.checkout_head(head.into());
 
     // Cleanup
-    // let _remove_tmp_dir = fs::remove_dir_all(tmp_path);
+    let _remove_tmp_dir = fs::remove_dir_all(tmp_path);
 }
 
 // TODO: Look at changing &Path to AsRef<Path>
@@ -83,27 +85,60 @@ fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry) -> io::Result<()>) -> io::Resul
 // dot_files_path: path to user's existing dotfiles directory
 // tmp_path: path to temporary directory where the repo is cloned
 
-fn rename_files(dot_files_path: &Path, tmp_path: &Path) -> io::Result<()> {
+// 7/10/24: Converting params to be PathBuf. Haven't converted the rest of
+//          the code to reflect this yet
+fn rename_files(dot_files_path: &PathBuf, tmp_path: &PathBuf) -> io::Result<()> {
     // Iterate over each item in tmp_path. Checking tmp_path first before
     // doing any renaming, as I don't want to rename/move existing files
     // that would not be affected by dotcomfy installation.
-    for entry in fs::read_dir(tmp_path)? {
+    for entry in fs::read_dir(tmp_path.as_path())? {
         let new_path = entry?.path();
-        // What am I even doing with `new_entry`?
-        if let new_entry = new_path.file_name() {
+        println!("{new_path:?}");
+        //let new_entry = new_path.file_name()?.to_os_string();
+        if let Some(new_entry) = &new_path.file_name() {
+            let old_path = append_to_path(&dot_files_path, &new_entry);
+            println!("{old_path:?}");
             // At this point, `new_entry` is an OsStr
             // Want to check to see if new_entry has a corresponding entry
             // in dot_files_path. If so, rename corresponding entry to
             // {corresponding_entry}.pre-dotcomfy, put new_entry in its place.
-        };
-        // I think I need to move the following logic into the new_entry block.
-        let old_path = dot_files_path.join(new_path.as_path());
-        match old_path.try_exists() {
-            Ok(true) => fs::rename(old_path, new_path),
-            Ok(false) => fs::rename(old_path, new_path),
-            Err(e) => panic!("Failed to rename: {}", e),
-        }?;
-        println!("{entry:?}");
+            // I think I need to move the following logic into the new_entry block.
+            // match old_path.try_exists() {
+            //     Ok(true) => fs::rename(old_path, new_path),
+            //     Ok(false) => fs::rename(old_path, new_path),
+            //     Err(e) => panic!("Failed to rename: {}", e),
+            // }?;
+        }
     }
     Ok(())
+}
+
+fn rename_files_recursively(dot_files_path: &PathBuf, tmp_path: &PathBuf) -> io::Result<()> {
+    // Skip empty directories
+    for entry in WalkDir::new(tmp_path).min_depth(1) {
+        let new_path = entry?.path();
+        println!("{new_path:?}");
+        //let new_entry = new_path.file_name()?.to_os_string();
+        if let Some(new_entry) = &new_path.file_name() {
+            let old_path = append_to_path(&dot_files_path, &new_entry);
+            println!("{old_path:?}");
+            // At this point, `new_entry` is an OsStr
+            // Want to check to see if new_entry has a corresponding entry
+            // in dot_files_path. If so, rename corresponding entry to
+            // {corresponding_entry}.pre-dotcomfy, put new_entry in its place.
+            // I think I need to move the following logic into the new_entry block.
+            // match old_path.try_exists() {
+            //     Ok(true) => fs::rename(old_path, new_path),
+            //     Ok(false) => fs::rename(old_path, new_path),
+            //     Err(e) => panic!("Failed to rename: {}", e),
+            // }?;
+        }
+    }
+    Ok(())
+}
+
+fn append_to_path(p: impl Into<OsString>, s: impl AsRef<OsStr>) -> PathBuf {
+    let mut p = p.into();
+    p.push(s);
+    p.into()
 }
